2dd52d4998a61e28ec5094e016c99df6e9046e14
aqw00 <matheus_tone@hotmail.com>
2023-03-03 16:04:16 +0100

    Cleaned the code a little and wrote a TODO

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,17 @@
 public class Main {
     public static void main(String[] args) {
 
-
-
+        // TODO: make a way to select the website that you want to login
+        String url = "https://www.facebook.com/";
         String email = ReadPasswordFile.readFile().get(0)[0];
         String password = ReadPasswordFile.readFile().get(0)[1];
-
-
-
-        /*
-            TONE:
-                - "/home/tone/IdeaProjects/practice-enterprise/instalation/chromedriver_linux64/chromedriver"
-            ROBIN:
-                - "C:\\Users\\robin\\OneDrive\\Documenten\\Thomas More\\Semester2\\Practice Enterprise\\practice-enterprise\\chromedriver_win32\\chromedriver.exe"
+        /* TONE */
+        String pathChrome = "/home/tone/IdeaProjects/practice-enterprise/instalation/chromedriver_linux64/chromedriver";
+        /* ROBIN
+        String pathChrome = "C:\\Users\\robin\\OneDrive\\Documenten\\Thomas More\\Semester2\\Practice Enterprise\\practice-enterprise\\chromedriver_win32\\chromedriver.exe";
          */
 
-        FacebookPageImporter.importFacebookPage("https://www.facebook.com/",email, password,
-                "C:\\Users\\robin\\OneDrive\\Documenten\\Thomas More\\Semester2\\Practice Enterprise\\practice-enterprise\\chromedriver_win32\\chromedriver.exe");
+        FacebookPageImporter.importFacebookPage(url,email, password, pathChrome);
 
 
     }
