74df035c25a0e13375ccae583b4ef91fa2c09b8f
aqw00 <matheus_tone@hotmail.com>
2023-03-24 16:04:44 +0100

    Added links and positions for the websites

--- src/FacebookPageImporter.java
+++ src/FacebookPageImporter.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class FacebookPageImporter {
-    public static void importFacebookPage(String url, String email, String password, String pathChromedriver) {
+    public static void importFacebookPage(String url, String email, String password, String pathChromedriver, String usernamePosition, String passwordPosition, String buttonPosition) {
 
 
         //ReadPasswordFile.readFile();
@@ -13,24 +13,27 @@ public class FacebookPageImporter {
 
         // Create a new instance of the Chrome driver
         System.setProperty("webdriver.chrome.driver", pathChromedriver);
+        /*
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-notifications");
         WebDriver driver = new ChromeDriver(options);
-
+        */
+        WebDriver driver = new ChromeDriver();
         // Navigate to the Facebook login page
         driver.get(url);
 
         // Find the email input field and set its value
-        WebElement emailInput = driver.findElement(By.name("email"));
+        WebElement emailInput = driver.findElement(By.name(usernamePosition));
         emailInput.sendKeys(email);
 
         // Find the password input field and set its value
-        WebElement passwordInput = driver.findElement(By.name("pass"));
+        WebElement passwordInput = driver.findElement(By.name(passwordPosition));
         passwordInput.sendKeys(password);
 
         // Submit the login form
-        WebElement loginButton = driver.findElement(By.name("login"));
-        loginButton.submit();
+        WebElement loginButton = driver.findElement(By.name(buttonPosition));
+        //loginButton.submit();
+
 
         // Wait for the login process to complete
         try {
@@ -38,7 +41,7 @@ public class FacebookPageImporter {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
+        /*
         // Navigate to the Facebook page to import
         driver.get("https://www.facebook.com/pages/import");
 
@@ -48,10 +51,10 @@ public class FacebookPageImporter {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
+        */
         // Import the page
         WebElement importButton = driver.findElement(By.xpath("//button[contains(text(), 'Import Page')]"));
-        importButton.click();
+        //importButton.click();
 
         // Close the browser
         driver.quit();
--- src/Main.java
+++ src/Main.java
@@ -12,24 +12,24 @@ public class Main {
         String username = "sftpuser";
         String SFTP_password = "W@chtwoord"; // TODO: find a way to hide this
         String remoteFilePath = "/shared/test.csv";
-        //String localFilePath = "/home/tone/IdeaProjects/practice-enterprise/src/test.csv";
+        String localFilePath = "/home/tone/IdeaProjects/practice-enterprise/src/test.csv";
 
         //Connection.SFTPConnect.connectSFTP(host, port, username ,SFTP_password, remoteFilePath, localFilePath);
 
-        String url = "https://www.facebook.com/";
+        String url = "";
 
         String email = Reading.ReadPasswordFile.readFile().get(0)[0];
         String password = Reading.ReadPasswordFile.readFile().get(0)[1];
         String browser = Reading.ReadPasswordFile.readFile().get(0)[2];
-        /* TONE
+        /* TONE*/
         String pathChrome = "/home/tone/IdeaProjects/practice-enterprise/instalation/linux/chromedriver_linux64/chromedriver";
-         */
+
         /* ROBIN*/
         // String pathChrome = "C:\\Users\\robin\\OneDrive\\Documenten\\Thomas More\\Semester2\\Practice Enterprise\\practice-enterprise\\chromedriver_win32\\chromedriver.exe";
 
 
         // UI.ApplicationUI.AccessHTMLPage(pathChrome); //start ui html
-        //FacebookPageImporter.importFacebookPage(url,email, password, pathChrome); start driver with facebook
+        //FacebookPageImporter.importFacebookPage(url,email, password, pathChrome);
 
         // Create a JFrame and set its size
         JFrame frame = new JFrame("My UI");
@@ -59,26 +59,18 @@ public class Main {
         JButton btnThomasMore = new JButton("Thomas More");
         panel.add(btnThomasMore);
 
-        JButton btnSpotify = new JButton("Spotify");
-        panel.add(btnSpotify);
-
-        JButton btnSteam = new JButton("Steam");
-        panel.add(btnSteam);
-
         JButton btnOutlook = new JButton("Outlook");
         panel.add(btnOutlook);
 
         // Add ActionListeners to the buttons
-        btnFacebook.addActionListener(e -> System.out.println("Choice: Facebook!"));
-        btnGoogle.addActionListener(e -> System.out.println("Choice: Google!"));
+        btnFacebook.addActionListener(e -> FacebookPageImporter.importFacebookPage("https://www.facebook.com/",email, password, pathChrome, "email", "pass", "login"));
+        btnGoogle.addActionListener(e -> FacebookPageImporter.importFacebookPage("https://accounts.google.com/ServiceLogin?hl=NL",email, password, pathChrome, "identifier", "Passwd", "login"));
         btnAmazon.addActionListener(e -> System.out.println("Choice: Amazon!"));
-        btnGitHub.addActionListener(e -> System.out.println("Choice: GitHub!"));
-        btnInstagram.addActionListener(e -> System.out.println("Choice: Instagram!"));
-        btnNetflix.addActionListener(e -> System.out.println("Choice: Netflix!"));
-        btnThomasMore.addActionListener(e -> System.out.println("Choice: Thomas More!"));
-        btnSpotify.addActionListener(e -> System.out.println("Choice: Spotify!"));
-        btnSteam.addActionListener(e -> System.out.println("Choice: Steam!"));
-        btnOutlook.addActionListener(e -> System.out.println("Choice: Outlook!"));
+        btnGitHub.addActionListener(e -> FacebookPageImporter.importFacebookPage("https://github.com/login",email, password, pathChrome, "login", "password", "login"));
+        btnInstagram.addActionListener(e -> FacebookPageImporter.importFacebookPage("https://www.instagram.com/",email, password, pathChrome, "username", "password", "login"));
+        btnNetflix.addActionListener(e -> FacebookPageImporter.importFacebookPage("https://www.netflix.com/nl/login",email, password, pathChrome, "userLoginId", "password", "login"));
+        btnThomasMore.addActionListener(e -> FacebookPageImporter.importFacebookPage("https://thomasmore365.sharepoint.com/sites/s.eict.denayer/SitePages/Home.aspx",email, password, pathChrome, "loginfmt", "passwd", "login"));
+        btnOutlook.addActionListener(e -> FacebookPageImporter.importFacebookPage("https://outlook.live.com/owa/?nlp=1",email, password, pathChrome, "loginfmt", "passwd", "login"));
 
         // Add the JPanel to the JFrame and make it visible
         frame.add(panel);
