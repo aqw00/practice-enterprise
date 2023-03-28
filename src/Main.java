be32752d3ce600fb15b9660416ed740b082ad3b0
aqw00 <matheus_tone@hotmail.com>
2023-03-28 12:24:53 +0200

    Merged the program so that we have a "complete version"

--- src/Main.java
+++ src/Main.java
@@ -14,13 +14,10 @@ public class Main {
         String remoteFilePath = "/shared/test.csv";
         String localFilePath = "/home/tone/IdeaProjects/practice-enterprise/src/test.csv";
 
-        //Connection.SFTPConnect.connectSFTP(host, port, username ,SFTP_password, remoteFilePath, localFilePath);
 
         String url = "";
 
-        String email = Reading.ReadPasswordFile.readFile().get(0)[0];
-        String password = Reading.ReadPasswordFile.readFile().get(0)[1];
-        String browser = Reading.ReadPasswordFile.readFile().get(0)[2];
+
         /* TONE*/
         String pathChrome = "/home/tone/IdeaProjects/practice-enterprise/instalation/linux/chromedriver_linux64/chromedriver";
 
@@ -62,15 +59,28 @@ public class Main {
         JButton btnOutlook = new JButton("Outlook");
         panel.add(btnOutlook);
 
+        // TODO: add a way to select the correct credentials.
         // Add ActionListeners to the buttons
-        btnFacebook.addActionListener(e -> FacebookPageImporter.importFacebookPage("https://www.facebook.com/",email, password, pathChrome, "email", "pass", "login"));
-        btnGoogle.addActionListener(e -> FacebookPageImporter.importFacebookPage("https://accounts.google.com/ServiceLogin?hl=NL",email, password, pathChrome, "identifier", "Passwd", "login"));
+        btnFacebook.addActionListener(e -> {
+            Connection.SFTPConnect.connectSFTP(host, port, username ,SFTP_password, remoteFilePath, localFilePath);
+            for (int i = 0; i < Reading.ReadPasswordFile.readFile().size(); i++) {
+                if (Reading.ReadPasswordFile.readFile().get(i)[2].equals("facebook")) {
+                    String email = Reading.ReadPasswordFile.readFile().get(i)[0];
+                    String password = Reading.ReadPasswordFile.readFile().get(i)[1];
+                    String browser = "https://www.facebook.com/";
+                    FacebookPageImporter.importFacebookPage(browser, email, password, pathChrome, "email", "pass", "login");
+                } else {
+                    System.out.println("Sorry there is no login");
+                }
+            }
+        });
+        btnGoogle.addActionListener(e -> FacebookPageImporter.importFacebookPage("https://accounts.google.com/ServiceLogin?hl=NL","email", "password", pathChrome, "identifier", "Passwd", "login"));
         btnAmazon.addActionListener(e -> System.out.println("Choice: Amazon!"));
-        btnGitHub.addActionListener(e -> FacebookPageImporter.importFacebookPage("https://github.com/login",email, password, pathChrome, "login", "password", "login"));
-        btnInstagram.addActionListener(e -> FacebookPageImporter.importFacebookPage("https://www.instagram.com/",email, password, pathChrome, "username", "password", "login"));
-        btnNetflix.addActionListener(e -> FacebookPageImporter.importFacebookPage("https://www.netflix.com/nl/login",email, password, pathChrome, "userLoginId", "password", "login"));
-        btnThomasMore.addActionListener(e -> FacebookPageImporter.importFacebookPage("https://thomasmore365.sharepoint.com/sites/s.eict.denayer/SitePages/Home.aspx",email, password, pathChrome, "loginfmt", "passwd", "login"));
-        btnOutlook.addActionListener(e -> FacebookPageImporter.importFacebookPage("https://outlook.live.com/owa/?nlp=1",email, password, pathChrome, "loginfmt", "passwd", "login"));
+        btnGitHub.addActionListener(e -> FacebookPageImporter.importFacebookPage("https://github.com/login","email", "password", pathChrome, "login", "password", "login"));
+        btnInstagram.addActionListener(e -> FacebookPageImporter.importFacebookPage("https://www.instagram.com/","email", "password", pathChrome, "username", "password", "login"));
+        btnNetflix.addActionListener(e -> FacebookPageImporter.importFacebookPage("https://www.netflix.com/nl/login","email", "password", pathChrome, "userLoginId", "password", "login"));
+        btnThomasMore.addActionListener(e -> FacebookPageImporter.importFacebookPage("https://thomasmore365.sharepoint.com/sites/s.eict.denayer/SitePages/Home.aspx","email", "password", pathChrome, "loginfmt", "passwd", "login"));
+        btnOutlook.addActionListener(e -> FacebookPageImporter.importFacebookPage("https://outlook.live.com/owa/?nlp=1","email", "password", pathChrome, "loginfmt", "passwd", "login"));
 
         // Add the JPanel to the JFrame and make it visible
         frame.add(panel);
