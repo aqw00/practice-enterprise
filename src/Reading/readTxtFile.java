3129dda59a7ca55d18359a2f4129aca4c96d3699
aqw00 <matheus_tone@hotmail.com>
2023-04-25 10:25:37 +0200

    Made txt file reader

---
+++ src/Reading/readTxtFile.java
@@ -0,0 +1,38 @@
+package Reading;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class readTxtFile {
+    public static List<String> txtFileHandeling(String fileName, boolean readOrWrite, String newValue)
+    {
+        List<String> myArray = new ArrayList<>();
+        if(readOrWrite)
+        {
+            try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+                String line;
+                while ((line = br.readLine()) != null) {
+                    String value = line.trim();
+                    myArray.add(value);
+                }
+                return myArray;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return myArray;
+        }
+        else
+        {
+            try (FileWriter fw = new FileWriter(fileName, true)) {
+                fw.write(newValue + "\n");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+}
