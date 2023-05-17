721ee64b55686cb8c2b41de71134789e99d09f91
aqw00 <matheus_tone@hotmail.com>
2023-05-17 10:02:57 +0200

    Made better system for path's

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 import Frames.Login;
 import Frames.Website;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,14 +16,21 @@ public class Main {
         //String pathButtonWebsites = "/home/tone/IdeaProjects/practice-enterprise/src/buttonWebsites.txt";
         // String pathWebsiteInfo = "/home/tone/IdeaProjects/practice-enterprise/src/websiteInfo.csv";
 
-        String pathChrome = "C:\\Users\\aqw00\\IdeaProjects\\practice-enterprise\\instalation\\chromedriver.exe";
-        String pathButtonWebsites = "C:\\Users\\aqw00\\IdeaProjects\\practice-enterprise\\src\\buttonWebsites.txt";
-        String pathWebsiteInfo = "C:\\Users\\aqw00\\IdeaProjects\\practice-enterprise\\src\\websiteInfo.csv";
+        //String pathChrome = "C:\\Users\\aqw00\\IdeaProjects\\practice-enterprise\\instalation\\chromedriver.exe";
+        //String pathButtonWebsites = "C:\\Users\\aqw00\\IdeaProjects\\practice-enterprise\\src\\buttonWebsites.txt";
+        //String pathWebsiteInfo = "C:\\Users\\aqw00\\IdeaProjects\\practice-enterprise\\src\\websiteInfo.csv";
 
         /* ROBIN */
         //String pathChrome = "C:\\Users\\robin\\OneDrive\\Documenten\\Thomas More\\Semester2\\Practice Enterprise\\practice-enterprise\\chromedriver_win32\\chromedriver_win32\\chromedriver.exe";
         //String pathButtonWebsites = "C:\\Users\\robin\\IdeaProjects\\practice-enterprise\\src\\buttonWebsites.txt";
         //String pathWebsiteInfo = "C:\\Users\\robin\\IdeaProjects\\practice-enterprise\\src\\websiteInfo.csv";
+
+        /*TEST HOME*/
+        String basePath = "C:\\Users\\aqw00\\IdeaProjects\\practice-enterprise\\";
+
+        String pathChrome = basePath + "instalation\\chromedriver.exe";
+        String pathButtonWebsites = basePath + "src\\buttonWebsites.txt";
+        String pathWebsiteInfo = basePath + "src\\websiteInfo.csv";
 
         Login.loginFrame(pathButtonWebsites, pathWebsiteInfo, list, pathChrome);
         //Website.webFrame(pathButtonWebsites, pathWebsiteInfo, list, pathChrome);
